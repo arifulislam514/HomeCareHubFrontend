464a3e8a419bc7ace92983338db5b06305308f94
arifulislam514 <ariful425616@gmail.com>
2025-09-30 18:34:25 +0600

    fix import

--- src/layouts/UserDashboardLayout.jsx
+++ src/layouts/UserDashboardLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/Dashboard/User/sidebar";
+import Sidebar from "../components/Dashboard/User/Sidebar";
 
 const DashboardLayout = () => {
   return (
