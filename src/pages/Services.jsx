3604c746e70477e0f96063e5448674f402878d08
arifulislam514 <ariful425616@gmail.com>
2025-09-30 18:29:44 +0600

    fix import

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ExcellentService from '../components/Products/ExcellentService';
 import BookingSection from '../components/Products/BookingSection';
-import FaqSection from '../components/Products/FaqSection';
+import FaqSection from '../components/Products/FAQSection';
 import PartnersSection from '../components/Products/PartnersSection';
 import PageBanner from '../layouts/PageBanner';
 
