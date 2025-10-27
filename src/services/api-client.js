51cc1d56c5b7c2c6294d879866de84f0f811a533
arifulislam514 <ariful425616@gmail.com>
2025-10-27 22:30:01 +0600

    base url change

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: "https://home-care-hub.vercel.app/api/v1",
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL: "https://home-care-hub.vercel.app/api/v1",
+  // baseURL: "http://127.0.0.1:8000/api/v1/",
 });
 
 export default apiClient;
